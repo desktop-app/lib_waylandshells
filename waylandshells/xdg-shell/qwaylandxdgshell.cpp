8f98b8f1c13ea862a417271cbab08ad2cb91ee7c
Ilya Fedin <fedin-ilja2010@ya.ru>
2022-03-10 12:50:27 +0400

    Synchronize xdg-shell with current 6.2 branch state

--- waylandshells/xdg-shell/qwaylandxdgshell.cpp
+++ waylandshells/xdg-shell/qwaylandxdgshell.cpp
@@ -98,6 +98,9 @@ void QWaylandXdgSurface::Toplevel::applyConfigure()
     // TODO: none of the other plugins send WindowActive either, but is it on purpose?
     Qt::WindowStates statesWithoutActive = m_pending.states & ~Qt::WindowActive;
 
+#if QT_VERSION >= QT_VERSION_CHECK(6, 2, 0)
+    m_xdgSurface->m_window->handleToplevelWindowTilingStatesChanged(m_toplevelStates);
+#endif
     m_xdgSurface->m_window->handleWindowStatesChanged(statesWithoutActive);
 
     if (m_pending.size.isEmpty()) {
@@ -356,15 +359,16 @@ bool QWaylandXdgSurface::handleExpose(const QRegion &region)
 
 void QWaylandXdgSurface::applyConfigure()
 {
-    Q_ASSERT(m_pendingConfigureSerial != 0);
+    // It is a redundant ack_configure, so skipped.
+    if (m_pendingConfigureSerial == m_appliedConfigureSerial)
+        return;
 
     if (m_toplevel)
         m_toplevel->applyConfigure();
+    m_appliedConfigureSerial = m_pendingConfigureSerial;
 
     m_configured = true;
-    ack_configure(m_pendingConfigureSerial);
-
-    m_pendingConfigureSerial = 0;
+    ack_configure(m_appliedConfigureSerial);
 }
 
 bool QWaylandXdgSurface::wantsDecorations() const
@@ -459,6 +463,10 @@ void QWaylandXdgSurface::setPopup(QWaylandWindow *parent)
     positioner->set_anchor(QtWayland::xdg_positioner::anchor_top_left);
     positioner->set_gravity(QtWayland::xdg_positioner::gravity_bottom_right);
     positioner->set_size(m_window->geometry().width(), m_window->geometry().height());
+    positioner->set_constraint_adjustment(QtWayland::xdg_positioner::constraint_adjustment_slide_x
+        | QtWayland::xdg_positioner::constraint_adjustment_slide_y
+        | QtWayland::xdg_positioner::constraint_adjustment_flip_x
+        | QtWayland::xdg_positioner::constraint_adjustment_flip_y);
     m_popup = new Popup(this, parentXdgSurface, positioner);
     positioner->destroy();
     delete positioner;
